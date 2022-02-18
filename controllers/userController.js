820b209d74889c5ffee6ba2db963295d7b6b6ab3
Mohit Gupta <mohit0873.cse19@chitkara.edu.in>
2022-02-18 16:06:08 +0530

    Sign up authorized

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,41 @@
+const User=require('../models/user');
 module.exports.profile=function(req,res){
   res.render('user',{title:'Users'});
 };
+
+module.exports.signUp=function(req,res){
+  return res.render('sign-up',{title:'Codeial|Sign Up'});
+}
+
+module.exports.signIn=function(req,res){
+  return res.render('sign-in',{title:'Codeial|Sign In'});
+}
+
+//get Sign Up Data
+module.exports.create=function(req,res){
+  //TODO
+  //check if password and confirm password is not matching
+  if(req.body.password!=req.body.confirm_password){
+    return res.redirect('back');
+  }
+
+  //check if email already exists in database
+  User.findOne({email:req.body.email},function(err,user){
+      if(err){console.log("Error in finding user in database");return;}
+      if(!user){
+        User.create(req.body,function(err,user){
+          if(err){console.log("Error in creating user in database");return;}
+          return res.redirect('sign-in');
+        })
+      }
+      else{
+           return res.redirect('back');
+      }
+  })
+
+}
+
+//Sign In and Session Creation
+module.exports.createSession=function(req,res){
+  //TODO
+}
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,9 @@ const userController=require('../controllers/userController');
 
 Route.get('/profile',userController.profile);
 
+Route.get('/sign-up',userController.signUp);
+
+Route.get('/sign-in',userController.signIn);
+
+Route.post('/create',userController.create);
 module.exports=Route;
