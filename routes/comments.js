1585473a20cc089fd438c80f3f898702af7443c2
Mohit Gupta <mohit0873.cse19@chitkara.edu.in>
2022-03-03 16:22:30 +0530

    Used partials for less code in home.ejs

--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const passport=require('passport');
 const CommentsController=require('../controllers/commentController');
 
 Route.post('/create',passport.checkAuthentication,CommentsController.create);
+Route.get('/destroy/:id',passport.checkAuthentication,CommentsController.destroy);
 
 module.exports=Route;
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const passport=require('passport');
 const PostsController=require('../controllers/postController');
 
 Route.post('/create',passport.checkAuthentication,PostsController.create);
+Route.get('/destroy/:id',passport.checkAuthentication,PostsController.destroy);
 
 module.exports=Route;
